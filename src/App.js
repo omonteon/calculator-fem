9db06cd53b04044682a7505168c3c4ade2379c19
Omar Monteon <monteon.omar@gmail.com>
2021-06-19 15:43:01 -0500

    feat: Initial code for the App component. feat: Basic styling for the number pad.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Calculator from "./components/Calculator";
+import './App.css';
 
 const App = () => {
   return (
-   <h1>React app</h1>
+   <Calculator />
   );
 };
 
