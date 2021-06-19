e98fc6a66acbf3915295a70daa15720ab6fa6b98
Omar Monteon <monteon.omar@gmail.com>
2021-06-19 15:42:18 -0500

    feat: Initial code for Calculator component.

---
+++ src/components/Calculator.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import NumberPad from './NumberPad'
+
+function Calculator() {
+  return (
+    <div>
+      <NumberPad />
+    </div>
+  )
+}
+
+export default Calculator;
