d3fbaaa22dd23ab30a007b42bd7da51caf5ad3c9
Omar Monteon <monteon.omar@gmail.com>
2021-06-22 20:27:03 -0500

    feat: Add font import. feat: Add styling for calculator header. feat: Add header to calculator.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Display from "./Display";
 import NumberPad from "./NumberPad";
+import Switch from "./Switch";
 
-function Calculator({ displayValue = 0 }) {
+function Calculator({ displayValue = 399981 }) {
   return (
     <div className="calculator">
+      <div className="calculator__header">
+        <h2>calc</h2>
+        <Switch />
+      </div>
       <Display value={displayValue} />
       <NumberPad />
     </div>
