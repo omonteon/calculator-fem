ebaad23d9c10260a5357627de8f11d0cef913f1e
Omar Monteon <monteon.omar@gmail.com>
2021-06-26 19:59:23 -0500

    feat: Add theme picker title. chore: Rename CSS classes.

--- src/components/ThemeSwitch.js
+++ src/components/ThemeSwitch.js
@@ -3,54 +3,57 @@ import React, { useState } from "react";
 function ThemeSwitch() {
   const [theme, setTheme] = useState("1");
   return (
-    <div>
-      <div className="option-numbers">
-        <span>1</span>
-        <span>2</span>
-        <span>3</span>
-      </div>
-      <div className="theme-switch">
-        <div className="wrapper">
-          <input
-            type="radio"
-            name="theme"
-            id="theme-1"
-            value="1"
-            onChange={(e) => {
-              setTheme(e.currentTarget.value);
-            }}
-            checked={theme === "1"}
-            aria-checked={theme === "1" ? "true" : "false"}
-          />
-          <label htmlFor="theme-1"></label>
+    <div className="theme-picker">
+      <span className="theme-picker__title">Theme</span>
+      <div className="theme-picker__switch">
+        <div className="switch-option-numbers">
+          <span>1</span>
+          <span>2</span>
+          <span>3</span>
         </div>
-        <div className="wrapper">
-          <input
-            type="radio"
-            name="theme"
-            id="theme-2"
-            value="2"
-            onChange={(e) => {
-              setTheme(e.currentTarget.value);
-            }}
-            checked={theme === "2"}
-            aria-checked={theme === "2" ? "true" : "false"}
-          />
-          <label htmlFor="theme-2"></label>
-        </div>
-        <div className="wrapper">
-          <input
-            type="radio"
-            name="theme"
-            id="theme-3"
-            value="3"
-            onChange={(e) => {
-              setTheme(e.currentTarget.value);
-            }}
-            checked={theme === "3"}
-            aria-checked={theme === "3" ? "true" : "false"}
-          />
-          <label htmlFor="theme-3"></label>
+        <div className="triple-switch">
+          <div className="wrapper">
+            <input
+              type="radio"
+              name="theme"
+              id="theme-1"
+              value="1"
+              onChange={(e) => {
+                setTheme(e.currentTarget.value);
+              }}
+              checked={theme === "1"}
+              aria-checked={theme === "1" ? "true" : "false"}
+            />
+            <label htmlFor="theme-1"></label>
+          </div>
+          <div className="wrapper">
+            <input
+              type="radio"
+              name="theme"
+              id="theme-2"
+              value="2"
+              onChange={(e) => {
+                setTheme(e.currentTarget.value);
+              }}
+              checked={theme === "2"}
+              aria-checked={theme === "2" ? "true" : "false"}
+            />
+            <label htmlFor="theme-2"></label>
+          </div>
+          <div className="wrapper">
+            <input
+              type="radio"
+              name="theme"
+              id="theme-3"
+              value="3"
+              onChange={(e) => {
+                setTheme(e.currentTarget.value);
+              }}
+              checked={theme === "3"}
+              aria-checked={theme === "3" ? "true" : "false"}
+            />
+            <label htmlFor="theme-3"></label>
+          </div>
         </div>
       </div>
     </div>
