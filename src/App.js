1ccb95d383556d857a7b1087bec5f804e78bf886
Omar Monteon <monteon.omar@gmail.com>
2021-07-25 16:07:33 -0500

    Refactor: Moved theme switch to App component. Refactor: Moved attribution to App component. Refactor: Renamed css classes to match with correct component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import ThemeSwitch from "./components/ThemeSwitch";
 import Calculator from "./components/Calculator";
-import './App.css';
+import "./App.css";
 
-const App = () => {
+// In this project I'm learning/Using for the first time:
+// BEM
+// CSS Variables
+// CSS Grid
+// Figma
+
+// Resources that helped me with the project
+// https://webdesign.tutsplus.com/tutorials/how-to-make-custom-accessible-checkboxes-and-radio-buttons--cms-32074
+// https://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
+
+const THEME_BG_COLOR = { 1: "#3a4663", 2: "#E6E6E6", 3: "#17062a" };
+
+function App() {
+  const [theme, setTheme] = useState("1");
+
+  function handleThemeChange(theme) {
+    const root = window.document.documentElement;
+    root.style.setProperty("--background-color", THEME_BG_COLOR[theme]);
+    setTheme(theme);
+  }
   return (
-   <Calculator />
+    <>
+      <div className={`app theme-${theme}`}>
+        <div className="app__header">
+          <h2>calc</h2>
+          <ThemeSwitch theme={theme} onThemeChange={handleThemeChange} />
+        </div>
+        <Calculator />
+      </div>
+      <div className="attribution">
+        Challenge by
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="https://github.com/omonteon">Omar Monteon</a>.
+      </div>
+    </>
   );
-};
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,34 +1,21 @@
 import React, { useState } from "react";
 import Display from "./Display";
 import NumberPad from "./NumberPad";
-import ThemeSwitch from "./ThemeSwitch";
 import useStateMachine from "../utils/stateMachine";
 
-const THEME_BG_COLOR = { 1: "#3a4663", 2: "#E6E6E6", 3: "#17062a" };
-
 function Calculator() {
-  const [theme, setTheme] = useState("1");
   const [displayValue, setDisplayValue] = useState("0");
   const { dispatchInput } = useStateMachine(displayValue, setDisplayValue);
 
-  function handleThemeChange(theme) {
-    const root = window.document.documentElement;
-    root.style.setProperty("--background-color", THEME_BG_COLOR[theme]);
-    setTheme(theme);
-  }
   function handleNumberPadButtonClick(event) {
     const input = event.target.innerText;
     dispatchInput(input);
   }
   return (
-    <div className={`calculator theme-${theme}`}>
-      <div className="calculator__header">
-        <h2>calc</h2>
-        <ThemeSwitch theme={theme} onThemeChange={handleThemeChange} />
-      </div>
+    <>
       <Display value={displayValue} />
       <NumberPad onButtonClick={handleNumberPadButtonClick} />
-    </div>
+    </>
   );
 }
 
