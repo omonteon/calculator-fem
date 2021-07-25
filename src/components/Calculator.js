d5e6fe90a04fe49269b6ea84796af6a563c6c519
Omar Monteon <monteon.omar@gmail.com>
2021-07-25 15:27:15 -0500

    Refactor: Separate machine logic to a diff module. Refactors: Move button click logic to Calculator component.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,17 +2,23 @@ import React, { useState } from "react";
 import Display from "./Display";
 import NumberPad from "./NumberPad";
 import ThemeSwitch from "./ThemeSwitch";
+import useStateMachine from "../utils/stateMachine";
 
 const THEME_COLORS = { 1: "#3a4663", 2: "#E6E6E6", 3: "#17062a" };
 
 function Calculator() {
   const [theme, setTheme] = useState("1");
   const [displayValue, setDisplayValue] = useState("0");
+  const { dispatchInput } = useStateMachine(displayValue, setDisplayValue);
 
   function handleThemeChange(theme) {
     const root = window.document.documentElement;
     root.style.setProperty("--background-color", THEME_COLORS[theme]);
     setTheme(theme);
+  }
+  function handleNumberPadButtonClick(event) {
+    const input = event.target.innerText;
+    dispatchInput(input);
   }
   return (
     <div className={`calculator theme-${theme}`}>
@@ -21,10 +27,7 @@ function Calculator() {
         <ThemeSwitch theme={theme} onThemeChange={handleThemeChange} />
       </div>
       <Display value={displayValue} />
-      <NumberPad
-        displayValue={displayValue}
-        setDisplayValue={setDisplayValue}
-      />
+      <NumberPad onButtonClick={handleNumberPadButtonClick} />
     </div>
   );
 }
