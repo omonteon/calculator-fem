4267cac8451adcdbcb9d4c623a9ba0ffad557441
Omar Monteon <monteon.omar@gmail.com>
2021-07-25 16:34:14 -0500

    Feat: Format display value.

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Display({ value = 0 }) {
-  return <div className="display">{value}</div>;
+  return <div className="display">{parseFloat(value).toLocaleString()}</div>;
 }
 
 export default Display;
