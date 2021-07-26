f25685341632e04ca9c9ba47afc6918d999165a2
Omar Monteon <monteon.omar@gmail.com>
2021-07-26 15:44:47 -0500

    Feat: Set default theme on load.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,63 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import ThemeSwitch from "./components/ThemeSwitch";
 import Calculator from "./components/Calculator";
 import "./App.css";
 
-// In this project I'm learning/Using for the first time:
+// In this project I'm learning/using for the first time:
 // BEM
-// CSS Variables
+// CSS custom properties(variables)
 // CSS Grid
-// Figma
-
-// Resources that helped me with the project
-// https://webdesign.tutsplus.com/tutorials/how-to-make-custom-accessible-checkboxes-and-radio-buttons--cms-32074
-// https://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
 
 const THEME_BG_COLOR = { 1: "#3a4663", 2: "#E6E6E6", 3: "#17062a" };
 
 function App() {
-  const [theme, setTheme] = useState("1");
+  const defaultTheme = getDefaultTheme();
+  const [theme, setTheme] = useState(defaultTheme);
 
-  function handleThemeChange(theme) {
+  useEffect(() => {
+    localStorage.setItem("selected-theme", theme);
     const root = window.document.documentElement;
     root.style.setProperty("--background-color", THEME_BG_COLOR[theme]);
-    setTheme(theme);
+  }, [theme]);
+
+  function getDefaultTheme() {
+    const persistedThemePreference = localStorage.getItem("selected-theme");
+    const hasPersistedPreference = typeof persistedThemePreference === "string";
+
+    if (hasPersistedPreference) {
+      const root = window.document.documentElement;
+      root.style.setProperty(
+        "--background-color",
+        THEME_BG_COLOR[persistedThemePreference]
+      );
+      return persistedThemePreference;
+    }
+    try {
+      // If they haven't been explicit, let's check the media query
+      const preferedColorScheme = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+      );
+      const hasMediaQueryPreference =
+        typeof preferedColorScheme.matches === "boolean";
+
+      if (hasMediaQueryPreference) {
+        return preferedColorScheme.matches ? "1" : "2";
+      }
+    } catch (error) {
+      throw new Error("prefers-color-scheme is not supported by the browser");
+    }
+
+    // Default to Light
+    return "2";
   }
+
   return (
     <>
       <div className={`app theme-${theme}`}>
         <div className="app__header">
           <h2>calc</h2>
-          <ThemeSwitch theme={theme} onThemeChange={handleThemeChange} />
+          <ThemeSwitch theme={theme} onThemeChange={setTheme} />
         </div>
         <Calculator />
         <div className="attribution">
