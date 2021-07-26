434abbbd692e9db6c71ece8ab6d31ff1b38f9551
Omar Monteon <monteon.omar@gmail.com>
2021-07-26 14:18:51 -0500

    Fix: Ajust styles for long numbers to not overflow

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,14 +1,15 @@
 import React from "react";
 
 function Display({ value = 0 }) {
+  const formattedValue = parseFloat(value)
+    .toString()
+    .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
   return (
     <input
       type="text"
-      className="display"
+      className={`display ${formattedValue.length > 15 ? "long-number" : ""}`}
       readOnly={true}
-      value={parseFloat(value)
-        .toString()
-        .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}
+      value={formattedValue}
     />
   );
 }
